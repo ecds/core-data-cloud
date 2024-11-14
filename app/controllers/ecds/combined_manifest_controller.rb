a3baabf39a7c9f1891c09d879f6fa8d882662beb
CVVSAI <varshith.sai0605@gmail.com>
2024-11-14 13:49:40 -0500

    Added manifest controller

---
+++ app/controllers/ecds/combined_manifest_controller.rb
@@ -0,0 +1,44 @@
+module Ecds
+    class CombinedManifestController < ApplicationController
+      def show
+        # Get the main place record by UUID
+        record = CoreDataConnector::Place.find_by(uuid: params[:id])
+  
+        related_places_relationship_model_id = 6
+        photograph_relationship_uuid = '0fbeaac4-45a3-4767-b9bc-7674632a8be1'
+  
+        #Find the photograph manifest for the main record
+        photograph_manifest = record.manifests.find { |manifest| manifest.identifier.ends_with?(photograph_relationship_uuid) }
+  
+        # If no photograph manifest is found, return basic IIIF manifest
+        manifest = photograph_manifest.nil? ? generate_empty_manifest(record) : JSON.parse(photograph_manifest.content, symbolize_names: true)
+  
+        # Collect related places by fetching records from the related relationship
+        related_records_relationships = record.relationships.where(project_model_relationship_id: related_places_relationship_model_id)
+        related_records = related_records_relationships.map { |rel| CoreDataConnector::Place.find(rel.related_record_id) }
+  
+        related_records.each do |related_record|
+          related_photograph_manifest = related_record.manifests.find { |manifest| manifest.identifier.ends_with?(photograph_relationship_uuid) }
+          
+          next if related_photograph_manifest.nil?
+  
+          # Append items from each related photograph manifest to the main manifest
+          manifest[:items] += JSON.parse(related_photograph_manifest.content, symbolize_names: true)[:items]
+        end
+          render json: manifest
+      end
+  
+      private
+  
+      #Function to generate an empty manifest structure
+      def generate_empty_manifest(record)
+        {
+          id: record.uuid,
+          type: 'Manifest',
+          label: { en: [record.name] },
+          items: []
+        }
+      end
+    end
+  end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  namespace :ecds do
+    get 'manifest/:id', to: 'combined_manifest#show'
+  end
+  
   mount CoreDataConnector::Engine => '/core_data'
   mount UserDefinedFields::Engine, at: '/user_defined_fields'
 
